42dc40725f6fb22df0dda200e1b7c49e5735bd30
Kpytoo <88066233+Kpytoo@users.noreply.github.com>
2025-05-20 17:53:31 -0400

    Completed the project!

--- script.js
+++ script.js
@@ -8,13 +8,20 @@ let createGrid = (side_size) => {
     for(let c = 0; c < totalGrids; c++){
         let grid = document.createElement("div");
         grid.className = "grid";
-        grid.style.height = (500 / side_size) + "px";
-        grid.style.width = (500 / side_size) + "px";
+        grid.style.height = (100 / side_size) + "%";
+        grid.style.width = (100 / side_size) + "%";
         container.appendChild(grid);
     }
 }
 
-createGrid(100);
+let newGrid = (size_input) => {
+    while(container.lastChild){
+        container.removeChild(container.lastChild);
+    }
+    createGrid(size_input);
+}
+
+createGrid(16);//Initialize first 16x16 grid
 
 container.addEventListener("mouseover", (e) => {
     if(e.target.id !== "container"){ //So the border of the container doesn't change the color.
@@ -28,6 +35,12 @@ container.addEventListener("mouseover", (e) => {
 
 
 btn.addEventListener("click", (e) => {
-    let size_input = prompt("Enter the size of the grid (100 max)");
-    createGrid(size_input);
+    let size_input = prompt("Enter the number of squares per side for the new grid.\n(100 max)");
+    while(size_input > 100 || size_input < 1){
+        size_input = prompt("Please enter a number between 1 and 100!");
+    }
+    newGrid(size_input);
 });
+
+
+//7x7 and 13x13 not working
